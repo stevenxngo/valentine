e7d1500e248375f7ab0a4733c0df5234707ed0ea
stevenxngo <83997026+stevenxngo@users.noreply.github.com>
2024-02-05 12:43:32 -0500

    Add react and vite to footer

--- src/App.tsx
+++ src/App.tsx
@@ -80,8 +80,28 @@ function App() {
           </div>
         </>
       )}
-      <h6 className="mt-3 mb-0 pb-3">
-        developed by{" "}
+      <h6 className="mt-3">
+        built with{" "}
+        <a
+          className="footer-name"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://reactjs.org/"
+        >
+          react
+        </a>{" "}
+        and{" "}
+        <a
+          className="footer-name"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://vitejs.dev/"
+        >
+          vite
+        </a>
+      </h6>
+      <h6 className="mb-0 pb-3">
+        by{" "}
         <a
           className="footer-name"
           target="_blank"
